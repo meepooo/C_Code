e55a247d142bc51b3c4a9177c887ae14d5f271ff
meepooo <a741158582@163.com>
2019-02-02 22:30:54 +0800

    Update 第六章小结.h

--- 20190202__Code/第六章小结.h
+++ 20190202__Code/第六章小结.h
@@ -1,4 +1,4 @@
-//                   第六章小结：
+/*                   第六章小结：
 
 <>对象的生存期、作用域与可见性
 <>类的静态成员
@@ -6,3 +6,44 @@
 <>友元
 <>编译预处理命令
 <>多文件结构和工程
+
+例：数据的共享与保护
+
+程序结构
+数据共享
+作用域
+生存期
+
+一：作用域
+*/
+
+#include<iostream>
+using namespace std;
+
+void fn1();			//全局变量的作用域为文件作用域，
+int x=1;			//在整个程序运行期间有效
+int y=2;
+
+int main(){
+	cout<<"Begin..."<<endl;
+	cout<<"x="<<x<<endl;
+	cout<<"y="<<y<<endl;
+	
+	cout<<"Evaluate x and y in mian()..."<<endl;
+	
+	int x=10;		//如果在局部模块中声明了同名的变量，
+	int y=20;		//则在局部模块中，可见的是局部变量
+	cout<<"x="<<x<<endl;
+	cout<<"y="<<y<<endl;
+	
+	cout<<"setp into fn1()..."<<endl;
+	fn1();
+	
+	cout<<"Back in main "<<endl;
+	cout<<"x= "<<x<<endl;
+	cout<<"y="<<y<<endl;
+	
+	return 0;
+}
+
+
