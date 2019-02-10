b4901c5a27be768ae48d1c590d6c430e281f0b49
meepooo <a741158582@163.com>
2019-02-10 23:45:17 +0800

    Update 20190210—指针.h

--- 20190210_Code/20190210—指针.h
+++ 20190210_Code/20190210—指针.h
@@ -50,3 +50,14 @@ int main(){
 //i=10
 //*ptr =10
 
+#include<iostream>
+using namespace std;
+int main(){
+	//!void voidObject; 错，不能声明void类型的变量
+	void *pv;  //对，可以声明void类型的指针
+	int i=5;
+	pv =&i;				//void类型指针指向整型变量
+	int *pint = static_cast<int *>(pv);//void指针转换为int指针
+	cout<<"*pint = "<<*pint<<endl;
+	return 0;
+}
