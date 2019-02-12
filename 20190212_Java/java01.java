a250475c8e076882c3da7e0a97a917239dcb757c
meepooo <a741158582@163.com>
2019-02-12 22:51:18 +0800

    java

---
+++ 20190212_Java/java01.java
@@ -0,0 +1,14 @@
+/**
+* 测试while循环的用法
+*
+*/
+public class Test_20190212_while {
+	public static void main(String[] args) {
+		int a=1;							//循环初始化
+		while(a<=100) {						//循环条件判断
+			System.out.println(a);			//循环体
+			a++;							//迭代
+		}
+		System.out.println("循环结束");
+	}
+}
