cfbb08a1eb6d5050e9fb1165f2f3c1c3f05c7a58
meepooo <a741158582@163.com>
2019-02-12 23:05:06 +0800

    Create java02.java

---
+++ 20190212_Java/java02.java
@@ -0,0 +1,12 @@
+/*
+ * 测试for循环
+ * */
+public class Test_20190212_for {
+		public static void main(String[] args) {
+					//初始化；条件判断；迭代
+				for (int a=1; a<=100; a++) {
+					System.out.println(a);
+				}
+				System.out.println("循环结束");
+		}
+}
