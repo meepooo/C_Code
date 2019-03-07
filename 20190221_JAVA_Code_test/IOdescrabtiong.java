7b17c89466b1f3618a3bb1fb7f7e561c1c63b56c
meepooo <a741158582@163.com>
2019-03-07 23:02:52 +0800

    1

---
+++ 20190221_JAVA_Code_test/IOdescrabtiong.java
@@ -0,0 +1,17 @@
+package com.itheima_01;
+/*
+ * IO流分类：
+ * 			流向
+ * 					输入流	读取数据	FileReader	Reader
+ * 					输出流	写出数据	FileWriter	Writer
+ * 			数据类型
+ * 					字节流	
+ * 							字节输入流		读取数据	InputStream
+ * 							字节输出流		写出数据	OutputStream
+ * 					字符流
+ * 							字符输入流		读取数据	Reader
+ * 							字符输出流		写出数据	Writer
+ */
+public class IODemo {
+
+}
